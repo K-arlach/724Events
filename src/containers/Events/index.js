f15c3b38562086ddabdfb51f47579c8e973aa676
kyllianschneider <98122428+kyllianschneider@users.noreply.github.com>
2024-04-26 15:44:19 +0200

    Adding a type condition for the sorting

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -19,6 +19,7 @@ const EventList = () => {
       : data?.events) || []
   ).filter((event, index) => {
     if (
+      (!type || event.type === type) &&
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
     ) {

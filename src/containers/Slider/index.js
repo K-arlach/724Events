d2058b7a20dff9dae5dcf5f20a7c7d351bdf0c9d
kyllianschneider <98122428+kyllianschneider@users.noreply.github.com>
2024-04-28 23:43:56 +0200

    updating the dots

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,56 +5,53 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const Slider = () => {
-  const { data } = useData(Date);
-  const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 0
-  );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
-      5000
+    const { data } = useData();
+    const [index, setIndex] = useState(0);
+
+    const byDateDesc = data?.focus.sort((evtA, evtB) => (new Date(evtA.date) - new Date(evtB.date) ? -1 : 0));
+
+    const handleRadioChange = (radioIdx) => {
+        setIndex(radioIdx);
+    };
+
+    const nextCard = () => {
+        setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
+    };
+
+    useEffect(() => {
+        const timer = setTimeout(nextCard, 5000);
+        return () => clearTimeout(timer);
+    }, [index, byDateDesc]);
+
+    return (
+        <div className="SlideCardList">
+            {byDateDesc?.map((event, idx) => (
+                <div key={event.title} className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
+                    <img src={event.cover} alt="forum" />
+                    <div className="SlideCard__descriptionContainer">
+                        <div className="SlideCard__description">
+                            <h3>{event.title}</h3>
+                            <p>{event.description}</p>
+                            <div>{getMonth(new Date(event.date))}</div>
+                        </div>
+                    </div>
+                    <div className="SlideCard__paginationContainer">
+                        <div className="SlideCard__pagination">
+                            {byDateDesc.map((e, radioIdx) => (
+                                <input
+                                    key={`${e.date}`} // key propre à chaque dot
+                                    type="radio"
+                                    name={`radio-button-${radioIdx}`} // ajout d'un nom propre a chaque boutons
+                                    checked={idx === radioIdx}
+                                    onChange={() => handleRadioChange(radioIdx)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
     );
-  };
-  useEffect(() => {
-    nextCard();
-  });
-  
-  return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
-            </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
-    </div>
-  );
 };
 
 export default Slider;
